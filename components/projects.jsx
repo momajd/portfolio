7fabce008c495a1d74d7cf0c6799e2047ae56f3e
momajd <mo.a.majd@gmail.com>
2016-10-17 21:28:43 -0400

    css refactoring

--- components/projects.jsx
+++ components/projects.jsx
@@ -14,50 +14,54 @@ export default class Projects extends React.Component {
         transitionAppearTimeout={500}>
         <div className="projects-page">
           <h1 className="section-title">Projects</h1>
-          <div className="projects-container">
-            <div className="project">
-              <a href="http://insanfran.us" className="image">
-                <img src="http://i.imgur.com/y79QzAR.gif"/>
-              </a>
 
-              <div className="project-description">
-                <p className="project-title">
-                  <a href="http://insanfran.us">InsanFran</a>
-                  &nbsp;
-                  <i className="devicon-rails-plain"></i>
-                  <i className="devicon-react-original"></i>
-                  <i className="devicon-postgresql-plain"></i>
-                </p>
-                <hr/>
-                <p>InsanFran is a full-stack application for sharing San
-                  Francisco photography. It was inspired by Instagram. Built
-                  with Rails, React, Flux. RESTful JSON API on backend, reusable
-                  view components on frontend, and lots of other good stuff.
-                </p>
-              </div>
-            </div>
+          <div className="project">
+            <a href="http://insanfran.us" className="image">
+              <img src="http://i.imgur.com/y79QzAR.gif"/>
+            </a>
 
-            <div className="project">
-              <a href="http://ninetyninebridges.herokuapp.com" className="image">
-                <img src="images/99bridges.png"/>
-              </a>
-
-              <div className="project-description">
-                <p className="project-title">
-                  <a href="http://ninetyninebridges.herokuapp.com">99Bridges</a>
-                  &nbsp;
-                  <i className="devicon-rails-plain"></i>
-                  <i className="devicon-react-original"></i>
-                  <i className="devicon-postgresql-plain"></i>
-                </p>
-                <hr/>
-                <p>99Bridges is an original full-stack application that allows
-                  bridge engineers and enthusiasts to find and track information
-                  on bridges. Built with Ruby on Rails, React, Flux,
-                  React-bootstrap, Google Maps API </p>
-              </div>
+            <div className="project-description">
+              <p className="project-title">
+                <a href="http://insanfran.us">InsanFran</a>
+                &nbsp;
+                <i className="devicon-rails-plain"></i>
+                <i className="devicon-react-original"></i>
+                <i className="devicon-postgresql-plain"></i>
+              </p>
+              <hr/>
+              <p>InsanFran is a full-stack application for sharing San
+                Francisco photography. It was inspired by Instagram. Built
+                with Rails, React, Flux. RESTful JSON API on backend, reusable
+                view components on frontend, and lots of other good stuff.
+              </p>
             </div>
           </div>
+
+          <div className="project">
+            <a href="http://ninetyninebridges.herokuapp.com" className="image">
+              <img src="images/99bridges.png"/>
+            </a>
+
+            <div className="project-description">
+              <p className="project-title">
+                <a href="http://ninetyninebridges.herokuapp.com">99Bridges</a>
+                &nbsp;
+                <i className="devicon-rails-plain"></i>
+                <i className="devicon-react-original"></i>
+                <i className="devicon-postgresql-plain"></i>
+              </p>
+              <hr/>
+              <p>99Bridges is an original full-stack application that allows
+                bridge engineers and enthusiasts to find and track information
+                on bridges. Built with Ruby on Rails, React, Flux,
+                React-bootstrap, Google Maps API </p>
+            </div>
+          </div>
+
+          <div>
+            
+          </div>
+
         </div>
       </ReactCSSTransitionGroup>
     );
