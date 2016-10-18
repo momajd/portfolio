c712fd81fa181b4056094fe33c3b2ef03994118d
momajd <mo.a.majd@gmail.com>
2016-10-17 22:04:49 -0400

    projects

--- components/projects.jsx
+++ components/projects.jsx
@@ -58,8 +58,84 @@ export default class Projects extends React.Component {
             </div>
           </div>
 
-          <div>
-            
+          <div className="project">
+            <a href="http://momajd.github.io/sudoku-solver/" className="image">
+              <img src="http://i.imgur.com/KdErCOU.gif"/>
+            </a>
+
+            <div className="project-description">
+              <p className="project-title">
+                <a href="http://momajd.github.io/sudoku-solver/">Sudoku Solver</a>
+                &nbsp;
+                <i className="devicon-javascript-plain"></i>
+                <i className="devicon-html5-plain"></i>
+              </p>
+              <hr/>
+              	<p>A sudoku solver that uses a backtracking algorithm. Built
+                  with Javascript and HTML5 Canvas.
+                </p>
+            </div>
+          </div>
+
+          <div className="project">
+            <a href="http://momajd.github.io/Tron/" className="image">
+              <img src="http://i.imgur.com/yphZFFg.gif"/>
+            </a>
+
+            <div className="project-description">
+              <p className="project-title">
+                <a href="http://momajd.github.io/Tron/">Tron</a>
+                &nbsp;
+                <i className="devicon-javascript-plain"></i>
+                <i className="devicon-jquery-plain"></i>
+                <i className="devicon-html5-plain"></i>
+              </p>
+              <hr/>
+                <p>Remake of the classic arcade game using Javascript, jQuery,
+                  HTML5, and CSS. All rendering is done by manipulating HTML
+                  elements with jQuery
+                </p>
+            </div>
+          </div>
+
+          <div className="project">
+            <a href="http://momajd.github.io/Unbeatable-Tic-Tac-Toe" className="image">
+              <img src="images/tic-tac-toe.png"/>
+            </a>
+
+            <div className="project-description">
+              <p className="project-title">
+                <a href="http://momajd.github.io/Unbeatable-Tic-Tac-Toe">Unbeatable Tic-Tac-Toe</a>
+                &nbsp;
+                <i className="devicon-javascript-plain"></i>
+                <i className="devicon-react-original"></i>
+              </p>
+              <hr/>
+                <p>A tic-tac-toe game that can't be won. The computer player
+                  uses a poly-tree data stucture and depth-first-search
+                  algorithm to find the optimal moves. Built with JS and React.
+                </p>
+            </div>
+          </div>
+
+          <div className="project">
+            <a href="https://github.com/momajd/Rails-Lite" className="image">
+              <img src="images/rails_lite.png"/>
+            </a>
+
+            <div className="project-description">
+              <p className="project-title">
+                <a href="https://github.com/momajd/Rails-Lite">Rails Lite</a>
+                &nbsp;
+                <i className="devicon-ruby-plain"></i>
+              </p>
+              <hr/>
+                <p>Rails Lite is a web server MVC framework inspired by the
+                  functionality of Ruby on Rails. Built in Ruby. Parses requests
+                  and finds matching routes using regular expression, envokes a
+                  controller action, and constructs a response using ERB templating.
+                </p>
+            </div>
           </div>
 
         </div>
